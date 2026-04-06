1bf968100ffa19051743a447f6187a1129dd301a
Phong <phong1807@gmail.com>
2026-04-06 09:24:40 +0700

    edit 1

--- src/data/products.js
+++ src/data/products.js
@@ -7,7 +7,7 @@ export const products = [
   { id: '6', name: 'Bell Pepper Red', weight: '1kg', price: 4.99, category: 'Vegetables', image: require('../../assets/bellpepper.png') },
   { id: '7', name: 'Organic Bananas', weight: '12kg', price: 3.00, category: 'Fruits', image: require('../../assets/banana.png') },
   { id: '8', name: 'Ginger', weight: '250gm', price: 2.99, category: 'Vegetables', image: require('../../assets/ginger.png') },
-  { id: '9', name: 'Sprite Can', weight: '325ml', price: 1.50, category: 'Drinks', brands: 'Cocola',  image: require('../../assets/sprite.png') },
+  { id: '9', name: 'Sprite Can', weight: '325ml', price: 1.50, category: 'Drinks', image: require('../../assets/sprite.png') },
   { id: '10', name: 'Diet Coke', weight: '355ml', price: 1.99, category: 'Drinks', image: require('../../assets/dietcoke.png') },
   { id: '11', name: 'Apple & Grape Juice', weight: '2L', price: 15.50, category: 'Drinks', image: require('../../assets/applegrape.png') },
   { id: '12', name: 'Coca Cola Can', weight: '325ml', price: 4.99, category: 'Drinks', image: require('../../assets/coca.png') },
